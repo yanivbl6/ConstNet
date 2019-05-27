a2022113d05a8b63df8b515109a2df1160b1f7b2
valilenk <valilenk@gmail.com>
2019-05-27 13:05:49 +0200

    -pre-fn batchnorm, separate biases, conv init fix

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 """
 Wide ResNet by Sergey Zagoruyko and Nikos Komodakis
 Fixup initialization by Hongyi Zhang, Yann N. Dauphin, Tengyu Ma
-Based on code by xternalz and Andy Brock: 
+Based on code by xternalz and Andy Brock:
 https://github.com/xternalz/WideResNet-pytorch
 https://github.com/ajbrock/BoilerPlate
 """
@@ -24,26 +24,46 @@ class BasicBlock(nn.Module):
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.bn2 = nn.BatchNorm2d(out_planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
+        self.conv2 = nn.Conv2d(
+            out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
 
         self.equalInOut = in_planes == out_planes
-        self.conv_res = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False)
+        self.conv_res = nn.Conv2d(
+            in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False
+        )
         self.conv_res = not self.equalInOut and self.conv_res or None
 
-        assert self.use_fixup or self.use_bn, "Need to use at least one thing: Fixup or BatchNorm"
+        assert (
+            self.use_fixup or self.use_bn
+        ), "Need to use at least one thing: Fixup or BatchNorm"
 
         if self.use_fixup:
-            self.multiplicator = nn.Parameter(torch.ones(1,1,1,1))
-            self.biases = nn.ParameterList([nn.Parameter(torch.zeros(1,1,1,1))] * 4)
+            self.scale = nn.Parameter(torch.ones(1))
+            self.biases = nn.ParameterList(
+                [nn.Parameter(torch.zeros(1)) for _ in range(4)]
+            )
 
-            k = self.conv1.kernel_size[0] * self.conv1.kernel_size[1] * self.conv1.out_channels
-            self.conv1.weight.data.normal_(0, self.fixup_l ** (-0.5) * math.sqrt(2. / k)) 
+            k = (
+                self.conv1.kernel_size[0]
+                * self.conv1.kernel_size[1]
+                * self.conv1.out_channels
+            )
+            self.conv1.weight.data.normal_(
+                0, self.fixup_l ** (-0.5) * math.sqrt(2.0 / k)
+            )
             self.conv2.weight.data.zero_()
-            
+
             if self.conv_res is not None:
-                k = self.conv_res.kernel_size[0] * self.conv_res.kernel_size[1] * self.conv_res.out_channels
-                self.conv_res.weight.data.normal_(0, math.sqrt(2. / k))
+                k = (
+                    self.conv_res.kernel_size[0]
+                    * self.conv_res.kernel_size[1]
+                    * self.conv_res.out_channels
+                )
+                self.conv_res.weight.data.normal_(0, math.sqrt(2.0 / k))
 
     def forward(self, x):
         if self.use_bn:
@@ -58,7 +78,7 @@ class BasicBlock(nn.Module):
             out = self.relu(out) + self.biases[2]
             if self.droprate > 0:
                 out = F.dropout(out, p=self.droprate, training=self.training)
-            out = self.multiplicator * self.conv2(out) + self.biases[3]
+            out = self.scale * self.conv2(out) + self.biases[3]
 
         if self.equalInOut:
             return torch.add(x, out)
@@ -86,7 +106,15 @@ class NetworkBlock(nn.Module):
 
 
 class WideResNet(nn.Module):
-    def __init__(self, depth, num_classes, widen_factor=1, droprate=0.0, use_bn=True, use_fixup=False):
+    def __init__(
+        self,
+        depth,
+        num_classes,
+        widen_factor=1,
+        droprate=0.0,
+        use_bn=True,
+        use_fixup=False,
+    ):
         super(WideResNet, self).__init__()
 
         nChannels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
@@ -99,36 +127,47 @@ class WideResNet(nn.Module):
         BasicBlock.fixup_l = n * 3
         BasicBlock.use_fixup = use_fixup
         block = BasicBlock
-        
-        self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.conv1 = nn.Conv2d(
+            3, nChannels[0], kernel_size=3, stride=1, padding=1, bias=False
+        )
+        k = (
+            self.conv1.kernel_size[0]
+            * self.conv1.kernel_size[1]
+            * self.conv1.out_channels
+        )
+        self.conv1.weight.data.normal_(0, math.sqrt(2.0 / k))
 
         self.block1 = NetworkBlock(n, nChannels[0], nChannels[1], block, 1)
         self.block2 = NetworkBlock(n, nChannels[1], nChannels[2], block, 2)
         self.block3 = NetworkBlock(n, nChannels[2], nChannels[3], block, 2)
-        
-        self.bn1 = nn.BatchNorm2d(nChannels[3])
+
         self.relu = nn.ReLU(inplace=True)
         self.fc = nn.Linear(nChannels[3], num_classes)
         self.nChannels = nChannels[3]
 
+        self.fc.bias.data.zero_()
+        if use_fixup:
+            self.fc.weight.data.zero_()
+
+        k = (
+            self.conv1.kernel_size[0]
+            * self.conv1.kernel_size[1]
+            * self.conv1.out_channels
+        )
+        self.conv1.weight.data.normal_(0, math.sqrt(2.0 / k))
+
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                k = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / k))
-            elif isinstance(m, nn.BatchNorm2d):
+            if isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.bias.data.zero_()
-                if use_fixup:
-                    m.weight.data.zero_()
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.block1(out)
         out = self.block2(out)
         out = self.block3(out)
-        out = self.relu(self.bn1(out))
+        out = self.relu(out)
         out = F.avg_pool2d(out, 8)
         out = out.view(-1, self.nChannels)
         return self.fc(out)
